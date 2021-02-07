e049d762e188c681259e77c39c11eb8e7d962359
klakaash <akaashlkumar96@gmail.com>
2021-02-07 20:15:43 +0530

    weather completed

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,24 +1,81 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 import requests
-
+from .models import City 
+from .forms import CityForm
 # Create your views here.
 def index(request):
 
-    city_name= 'Bengaluru'
-    url= 'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={key}&units=metric'.format(
-        city_name=city_name, key='107f0bb3a6f90f8c582bfa6d95eda8da'
-    )
+    cities= City.objects.all()
+    er_msg=''
+    message=''
+    message_class=''
+    #city_name= 'Delhi'
+    if request.method =='POST':
+        form= CityForm(request.POST)
 
-    response=requests.get(url)
-    #print(response)
-    #print(response.json())
-    myjson=response.json()
+        if form.is_valid():
 
-    city_weather= {
-        'city':city_name,
-        'tempreature': myjson.get('main').get('temp'),
-        'description': myjson.get('weather')[0].get('description'),
-        'icon': myjson.get('weather')[0].get('icon'),
-    }
-    print(city_weather)
-    return render(request,'weather/weather.html')
+            new_city=form.cleaned_data['city_name']
+            existing_city_count= City.objects.filter(city_name=new_city).count()
+
+            if(existing_city_count==0):
+                url= 'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={key}&units=metric'.format(
+                city_name=new_city, key='107f0bb3a6f90f8c582bfa6d95eda8da'
+                )
+                response=requests.get(url)
+                myjson=response.json()
+                
+                if (myjson.get('cod')==200):
+                    form.save()
+                else:
+                    er_msg='City does not exist'
+            else:
+                er_msg='City already exists in DB'
+        if er_msg:
+            message=er_msg
+            message_class= 'is-danger'
+        else:
+            message= 'City added successfully '
+            message_class='is-success'
+
+
+    print(er_msg)
+    form = CityForm()   
+    weather_data=[]
+
+    for city in cities :
+        url= 'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={key}&units=metric'.format(
+        city_name=city, key='107f0bb3a6f90f8c582bfa6d95eda8da'
+        )
+
+        response=requests.get(url)
+        #print(response)
+        #print(response.json())
+        myjson=response.json()
+        #print(myjson)
+
+        city_weather= {
+            'city':city.city_name,
+            'tempreature': myjson.get('main',{}).get('temp'),
+            'description': myjson.get('weather')[0].get('description'),
+            'icon': myjson.get('weather')[0].get('icon'),
+        }
+
+        weather_data.append(city_weather)
+
+    #print(weather_data)
+    #print(city_weather)
+    context= {
+        'weather_data':weather_data, 
+        'form':form ,
+        'message':message,
+        'message_class':message_class
+        }
+    return render(request,'weather/weather.html',context)
+
+def delete_city(request,city_name):
+    City.objects.get(city_name=city_name).delete()
+    print('success deletion')
+
+
+    return redirect('home')
